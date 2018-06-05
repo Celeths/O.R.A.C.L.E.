e90d2b9264e9f3cfaf78e5c7f20075cf30f6dbf0
Leveles <tactrins@gmail.com>
2018-06-05 15:59:50 -0400

    Oracle For Discord X
    Lots of internal changes, and reworks. Error log is an addition to see each time the bot failed, to be able to improve its recognition much faster.

---
+++ Oracle For Discord X.py
@@ -0,0 +1,39 @@
+"""Operational Revelator And Change Likeliness Estimator - For Discord
+            Made by Leveles"""
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+import os
+import re
+import random
+from random import randint
+import asyncio
+import magic_conch_shell
+import vocab
+
+bot = commands.Bot(command_prefix="o!")
+
+client = discord.Client()
+
+@bot.event
+async def on_ready():
+    print("Active as " + bot.user.name + "\nWith the id " + bot.user.id + ".")
+
+
+@bot.command(pass_context=True)
+async def say(ctx):
+    global starter
+    name = ctx.message.author
+    msg = ctx.message.content.split(" ", 1)
+    response = msg[1]
+    response = str(response).lower()
+    starter = re.sub("[^\w]", " ",  response).split()
+    magic_conch_shell.teller(starter)
+    await bot.say(str(magic_conch_shell.answer))
+    if magic_conch_shell.error == "record":
+        manager = open("Error log", "a")
+        manager.write(str((name)) + " : " + response + "\n")
+        manager.close()
+
+#Token to run (KEEP SECRET)
+bot.run("DISCORD TOKEN")
---
+++ vocab.py
@@ -0,0 +1,27 @@
+#Basic sentance openers that signify a yes or no answer
+firstcheck = ("should", "can", "is", "will", "did", "do", "am", "are", "will", "was", "do")
+firstanswers = ("yes", "no", "maybe", "perhaps", "probably not", "not going to touch this one")
+
+#Outcomes from "what"
+secondcheck = ("what")
+nextSecondCheck = ("will", "is")
+nextSecondCheckAnswers = ("bad things", "nothing.", "you know this already", "good things",
+                          "complete destruction", "perfection")
+#Outcomes from "how"
+thirdCheck = ("how")
+nextThirdCheck = ("many", "much")
+nextThirdCheckTwo = ("will", "am", "are")
+nextThirdCheckTwoAnswers = ("with failure", "with success", "badly", "averagely", "amazingly")
+nextThirdCheckThree = ("more")
+nextThirdCheckThreeAnswers = ("many more", "just a few", "it never ends")
+nextThirdCheckAge = ("old")
+doCan = ("do", "can")
+doCanAnswers = ("it's not possible", "can't", "easily", "with difficulty")
+
+#Outcomes from "When"
+fourthCheck = ("when")
+fourthCheckAnswers = ("soon", "never", "in a while", "now", "today", "in a long time", "didn't it already happen")
+
+#Feelings
+feelings = ("good", "bad", "afraid", "angry", "sad", "happy", "joyous", "disgusted", "surprised", "depressed", "manic",
+            "strangely good", "almost dead", "anxious")
